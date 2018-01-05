387753f56b89e15722b9b6cabb28bbe183b8858d
Tangxing Zhou <zhoutangxing@126.com>
2018-01-05 11:04:35 +0800

    start uwsgi service

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 # python run_tests.py
+uwsgi --ini Lollipop/myweb_uwsgi.ini
 cp lollipopserver /etc/nginx/sites-available/
 ln -n /etc/nginx/sites-available/lollipopserver /etc/nginx/sites-enabled/lollipopserver
-nginx -s reload
+# nginx -s reload
+/etc/init.d/nginx restart
