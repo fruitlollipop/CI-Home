c052eb88f505657860f53517b06a645b2dd7e938
Tangxing Zhou <zhoutangxing@126.com>
2018-01-07 17:46:21 +0800

    rm shell

--- run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-current_dir=`pwd`
-firefox_driver_version="0.19.1"
-firefox_driver="geckodriver-v${firefox_driver_version}-linux64.tar.gz"
-wget https://github.com/mozilla/geckodriver/releases/download/v${firefox_driver_version}/$firefox_driver
-tar zxf $firefox_driver && chmod +x $firefox_driver
-export PATH=$PATH:$current_dir
-robot -o /var/lib/jenkins/logs ${current_dir}/st_login_start_page.robot
-exit 0
