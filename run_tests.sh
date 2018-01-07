38468c32e1a2bbeaefd337537062a97fc4c3c76f
Tangxing Zhou <zhoutangxing@126.com>
2018-01-07 17:05:07 +0800

    run robot case update

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,5 @@ firefox_driver = "geckodriver-v${firefox_driver_version}-linux64.tar.gz"
 wget https://github.com/mozilla/geckodriver/releases/download/v${firefox_driver_version}/$firefox_driver
 tar zxf $firefox_driver && chmod +x $firefox_driver
 export PATH=$PATH:$current_dir
-robot st_login_start_page.robot
+robot ${current_dir}/st_login_start_page.robot
 exit 0
