db666de107cddc9a3edd748418efb208365ac0cb
Tangxing Zhou <zhoutangxing@126.com>
2018-01-17 16:09:08 +0800

    v2.2.0 start app Strawberry and update the settings of DB

--- Lollipop/Lollipop/settings.py
+++ Lollipop/Lollipop/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Lollipop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '192.168.0.119',
+        'PORT': '8084',
+        'USER': 'root',
+        'PASSWORD': 'zhou19891001',
+        'NAME': '',
     }
 }
 
